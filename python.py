152a22819d93e679dd42e6e5cf7c908e658e2f8c
wondo92 <wondoy92@gmail.com>
2020-02-05 18:12:48 +0900

    파이썬올림

---
+++ python.py
@@ -0,0 +1,14 @@
+ coffee=10
+while True:
+	money = int(input("돈을 넣어주세요 :"))
+	if money == 300:
+		print('커피를 줍니다')
+		coffee = coffee - 1
+	elif money >300:
+		print('거스름돈 %d를 주고 커피는 줍니다.'%(money-300))
+		coffee = coffee - 1
+	else:
+		print('돈을 다시 돌려주고 커피를 주지 않습니다.')
+	if coffee == 0:
+		print("커피가 다 떨어졌습니다.')
+        break
