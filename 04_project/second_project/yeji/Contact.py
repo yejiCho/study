4ba2afb920b75bb5e54197a63566bdc05ae922c9
choyeji <goe152@naver.com>
2020-02-10 21:00:29 +0900

    [revise]refactoring_python_contact_program_ [add]class

--- 04_project/second_project/yeji/Contact.py
+++ 04_project/second_project/yeji/Contact.py
@@ -9,16 +9,22 @@
 class ContentError(Exception):
     def __str__(self):
         return "허용되지 않는 입력입니다."
+
+
 def input_content(content):
     if content == "":
         raise ContentError()
+
+
 def input_classification(classification,classifications):
     if classification not in classifications:
         raise ContentError()
+
 # class NumError(Exception):
 #     def __str__(self):
-#         return "숫자 1~5번만 입력하실 수 있습니다."
-# def input_num(self,num):
+#         return "1~5까지만 입력가능"
+#
+# def input_num(num):
 #     if num.isdigit() is False:
 #         raise NumError()
 
@@ -37,82 +43,91 @@ def main():
     """
     print(prompt)
 
+# def input_number(main_num):
+#     main_num = input("번호를 입력하세요.")
+#     try:
+#         input_num(main_num)
+#     except ValueError as errormessage:
+#         print(errormessage)
+#         return 0
 
-# 1번. 연락처 정보등록
-def input_contact(public_information):
-    print("등록할 회원의 정보를 입력하세요.")
-    name = input("이름: ").strip()
-    try:
-        input_content(name)
-    except ContentError as error_message:
-        print(error_message)
-        return {}
-    phone_number = input("전화번호(ex:01012345678): ").strip()
-    try:
-        input_content(phone_number)
-    except ContentError as error_message:
-        print(error_message)
-        return {}
-    classification = input("구분(ex:가족,친구,회사,기타):").strip()
-    classifications = ("가족","친구","회사","기타")
+class ContactProgram:
+    def __init__(self):
+        self.public_information = {}
+    # 1번. 연락처 정보등록
+    def input_contact(self):
+        print("등록할 회원의 정보를 입력하세요.")
+        name = input("이름: ").strip()
+        try:
+            input_content(name)
+        except ContentError as error_message:
+            print(error_message)
+            return {}
+        phone_number = input("전화번호(ex:01012345678): ").strip()
+        try:
+            input_content(phone_number)
+        except ContentError as error_message:
+            print(error_message)
+            return {}
+        classification = input("구분(ex:가족,친구,회사,기타):").strip()
+        classifications = ("가족","친구","회사","기타")
 
-    try:
-        input_classification(classification, classifications)
-    except ContentError as error_message:
-        print(error_message)
-        return {}
+        try:
+            input_classification(classification, classifications)
+        except ContentError as error_message:
+            print(error_message)
+            return {}
 
-    private_information = {
-        'name':name,
-        'phone_number':phone_number,
-        'classification':classification
-    }
-    if private_information:
-        public_information[private_information['phone_number']] = private_information
-    else:
-        print("정보가 없습니다.")
-    # return public_information
-    
-    return private_information
+        private_information = {
+            'name':name,
+            'phone_number':phone_number,
+            'classification':classification
+        }
+        if private_information:
+            self.public_information[private_information['phone_number']] = private_information
+        else:
+            print("정보가 없습니다.")
+        # return public_information
 
-
-# 2번. 연락처 목록조회
-def member_list(public_information):
-
-    print(f"총 {len(public_information)}명의 회원이 저장되어 있습니다.")
-    for phone_key in public_information.keys():
-        # print(phone_key)
-        print(f"이름={public_information[phone_key]['name']},"
-                f"전화번호={public_information[phone_key]['phone_number']},"
-                f"구분={public_information[phone_key]['classification']}")
+        return private_information
 
 
-# 3,4번. 수정,삭제
-def check_information(public_information,correct_comment):
+    # 2번. 연락처 목록조회
+    def member_list(self):
 
-    input_name = input(f"{correct_comment}이름을 입력하세요.")
+        print(f"총 {len(self.public_information)}명의 회원이 저장되어 있습니다.")
+        for phone_key in self.public_information.keys():
+            # print(phone_key)
+            print(f"이름={self.public_information[phone_key]['name']},"
+                    f"전화번호={self.public_information[phone_key]['phone_number']},"
+                    f"구분={self.public_information[phone_key]['classification']}")
 
-    keys = list(public_information.keys())
-    list_input_name = []
-    for key in keys:
-        if input_name == public_information[key]['name']:
-            list_input_name.append(key)
-    print(f'총{len(list_input_name)}개의 목록이 검색되었습니다.')
-    # 이름이 같은 애들의 목록을 전체 조회해준다.
-    for key in keys:
-        if input_name == public_information[key]['name']:
-            print(f"{str(keys.index(key)+1)}."
-                f"이름={public_information[key]['name']},"
-                f"전화번호={public_information[key]['phone_number']},"
-                f"구분={public_information[key]['classification']}")
-            print(key)
 
-    input_num = input(f"{correct_comment}할 번호를 입력하세요")
-    del public_information[list_input_name[int(input_num)-1]]
-    # print(public_information)
+    # 3,4번. 수정,삭제
+    def check_information(self,correct_comment):
 
-    if correct_comment == "삭제":
-        print(f"{correct_comment}가 완료되었습니다.")
-    else:
-        input_contact(public_information)
-        print(f"{correct_comment}이 완료되었습니다.")
+        input_name = input(f"{correct_comment}이름을 입력하세요.")
+
+        keys = list(self.public_information.keys())
+        list_input_name = []
+        for key in keys:
+            if input_name == self.public_information[key]['name']:
+                list_input_name.append(key)
+        print(f'총{len(list_input_name)}개의 목록이 검색되었습니다.')
+        # 이름이 같은 애들의 목록을 전체 조회해준다.
+        for key in keys:
+            if input_name == self.public_information[key]['name']:
+                print(f"{str(keys.index(key)+1)}."
+                    f"이름={self.public_information[key]['name']},"
+                    f"전화번호={self.public_information[key]['phone_number']},"
+                    f"구분={self.public_information[key]['classification']}")
+
+        input_num = input(f"{correct_comment}할 번호를 입력하세요")
+        del self.public_information[list_input_name[int(input_num)-1]]
+        # print(public_information)
+
+        if correct_comment == "삭제":
+            print(f"{correct_comment}가 완료되었습니다.")
+        else:
+            self.input_contact()
+            print(f"{correct_comment}이 완료되었습니다.")
--- 04_project/second_project/yeji/ContactMain.py
+++ 04_project/second_project/yeji/ContactMain.py
@@ -1,5 +1,7 @@
 # Contact 모듈에서 main,input_contact,member_list,check_information함수를 불러온다.
-from Contact import main,input_contact,member_list,check_information
+# from Contact import main,input_contact,member_list,check_information
+from Contact import ContactProgram
+from Contact import main
 import pickle
 # 초기 파일load시에 빈 텍스트파일일 경우에는 에러가 발생함
 # 그래서 예외처리를해줘야함
@@ -12,25 +14,22 @@ except:
 # main_num = 입력받을 번호 정의해줌
 main_num = ''
 public_info = {}
+contact = ContactProgram()
 while True:
     main()
     main_num = input("번호를 입력하세요. ")
     # try:
     #     main_num.isdigit()
-    # except ValueError:
-    #     print("1~5번까지만 입력받을수 있습니다.")
-    # try:
-    #     input_num(main_num)
-    # except NumError as error_message:
-    #     print(error_message)
+    # except:
+    #     print("1~5까지의 숫자만 입력하실수 있습니다.")
     if main_num == '1':
-        private_info = input_contact(public_info)
+        private_info = contact.input_contact()
     elif main_num == '2':
-        member_list(public_info)
+        contact.member_list()
     elif main_num == '3':
-        check_information(public_info,"수정")
+        contact.check_information("수정")
     elif main_num == '4':
-        check_information(public_info,"삭제")
+        contact.check_information("삭제")
     elif main_num == '5':
 # 마지막에 최종으로 남은 연락처가 txt파일에 저장되게 해놓음
         with open('contact.txt','wb') as file:
